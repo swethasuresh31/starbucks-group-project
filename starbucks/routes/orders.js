f9ec1bfc1edab0c96c4646a2ff858493815287b1
Akhila <akhila.sanka@sjsu.edu>
2019-05-05 22:20:46 -0700

    Orders response

--- starbucks/routes/orders.js
+++ starbucks/routes/orders.js
@@ -4,7 +4,19 @@ var router = express.Router();
 /* GET order */
 router.post('/', function(req, res, next) {
   //TODO
-  //order implementation
+  //order query
+
+  console.log("Orders: " + req.params.courseUid)
+  var query = ""
+  var params = []
+  connection.query(query, params, function (error, results, fields) {
+    if (error) {
+      console.log(error)
+      res.status(500).send(error);
+    } else {
+      res.status(200).send("Success");
+    }
+  });
 });
 
 module.exports = router;
