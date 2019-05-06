ccc90589bd0b72a13a9cbdddd524acceae9fa577
Vibha <vibhashree.ravindra@sjsu.edu>
2019-05-05 20:11:05 -0700

    Add card response

--- starbucks/routes/addcard.js
+++ starbucks/routes/addcard.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../db/connection')
 
 /* POST add card */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   //TODO
-  //add card implementation
+  //add card query
+
+  console.log("Adding card: " + req.params.courseUid)
+  var query = ""
+  var params = []
+  connection.query(query, params, function (error, results, fields) {
+    if (error) {
+      console.log(error)
+      res.status(500).send(error);
+    } else {
+      res.status(200).send("Success");
+    }
+  });
 });
 
 module.exports = router;
